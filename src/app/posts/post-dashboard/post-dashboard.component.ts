010fcadde8059dedb67f6cfde4c76dcbc179a62f
Brandon Dayauon <brdayauon@gmail.com>
2021-05-10 17:18:26 -0700

    fixed image problem

--- src/app/posts/post-dashboard/post-dashboard.component.ts
+++ src/app/posts/post-dashboard/post-dashboard.component.ts
@@ -51,8 +51,12 @@ export class PostDashboardComponent implements OnInit {
   }
 
   uploadImage(event){
+
+    // used to generate random ID for storage
+    const id = Math.random().toString(36).substring(2)
+    const path = 'posts/' + id
+
     const file = event.target.files[0]
-    const path = 'posts/${file.name}'
     
     if (file.type.split('/')[0] !== 'image'){
       return alert('only image files')
@@ -74,6 +78,17 @@ export class PostDashboardComponent implements OnInit {
       ).subscribe()
 
     }
+
+    // if (file.type.split('/')[0] !== 'image') {
+    //   return alert('only image files')
+    // } else {
+    //   const task = this.storage.upload(path, file)
+    //   this.downloadURL = task.downloadURL()
+    //   this.uploadPercent = task.percentageChanges()
+    //   console.log('Image Uploaded!')
+    //   this.downloadURL.subscribe(url => (this.image = url))
+    // }
+
   }
 
 }
